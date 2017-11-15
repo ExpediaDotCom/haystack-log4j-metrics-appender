fe3ec6ddceade3a818e86d4b4f421d55063f7b34
jeffbaker <jeffbaker@expedia.com>
2017-11-15 14:44:37 -0800

    Add copyright notices to Java files. Make minor changes to README.md.

--- src/main/java/com/expedia/www/haystack/metrics/appenders/log4j/EmitToGraphiteLog4jAppender.java
+++ src/main/java/com/expedia/www/haystack/metrics/appenders/log4j/EmitToGraphiteLog4jAppender.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Expedia, Inc.
+ *
+ *       Licensed under the Apache License, Version 2.0 (the "License");
+ *       you may not use this file except in compliance with the License.
+ *       You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *       Unless required by applicable law or agreed to in writing, software
+ *       distributed under the License is distributed on an "AS IS" BASIS,
+ *       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *       See the License for the specific language governing permissions and
+ *       limitations under the License.
+ *
+ */
 package com.expedia.www.haystack.metrics.appenders.log4j;
 
 import com.expedia.www.haystack.metrics.GraphiteConfig;
@@ -96,7 +112,8 @@ public class EmitToGraphiteLog4jAppender extends AbstractAppender {
 
             // It is possible but highly unlikely that two threads are in this if() block at the same time; if that
             // occurs, only one of the calls to ERRORS_COUNTERS.putIfAbsent(hashCode, counter) in the next line of code
-            // will succeed, and the increment of the thread whose call did not succeed will be lost.
+            // will succeed, but the increment of the thread whose call did not succeed will not be lost, because the
+            // value returned by this method will be the Counter put successfully by the other thread.
             ERRORS_COUNTERS.putIfAbsent(hashCode, counter);
         }
         return ERRORS_COUNTERS.get(hashCode);
